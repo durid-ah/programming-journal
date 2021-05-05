3fc31663a2dbd0142d3a31543611c3778fa985af
durid-ah <ahmad.durid96@gmail.com>
2021-05-04 19:03:37 -0600

    d3: initial preprocessing of line chart data

--- javascript/d3/line-chart.js
+++ javascript/d3/line-chart.js
@@ -3,9 +3,14 @@
 async function process_line_data() {
    let data = await fetch_data();
    let filtered_data = filterData(data);
-   
+   return filtered_data;
 }
 
 async function line_chart_main() {
+   // Setting up the frame measurements
+   const margin = {top: 40, bottom:40, left: 80, right: 40};
+   const width = 500 - margin.left - margin.right;
+   const height = 500 - margin.top - margin.bottom;
+
 
 }
