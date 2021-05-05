00d7a82721736ed822f44807e59f95adb8cbb541
durid-ah <ahmad.durid96@gmail.com>
2021-05-04 19:02:11 -0600

    d3: adding the line chart script to index.html

---
+++ javascript/d3/line-chart.js
@@ -0,0 +1,11 @@
+// line charts are paths where you tell the svg towards which point 
+
+async function process_line_data() {
+   let data = await fetch_data();
+   let filtered_data = filterData(data);
+   
+}
+
+async function line_chart_main() {
+
+}
